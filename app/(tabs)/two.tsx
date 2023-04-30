fd9cea2a68fc74661c919463f7d73876ca79022c
Matthew Meredith <merematt@udel.edu>
2023-04-30 03:17:52 -0400

    remove import

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,6 +1,5 @@
 import { ThemedView } from "../../components/Themed";
 import { Editor } from "../../components/ide/Editor";
-import { styles } from '../../constants/Style';
 
 export default function TabTwoScreen() {
     return (
